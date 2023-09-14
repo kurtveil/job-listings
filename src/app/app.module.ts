d80f4abf3dbb96de473c22e9c0cfd2cde3d9169b
kurt <kurtveil1@gmail.com>
2023-09-14 14:08:50 -0500

    [ADD] added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { FilterComponent } from './components/filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JobsComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
