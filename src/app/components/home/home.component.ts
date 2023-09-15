c319f217cb940028ac8c7cff9d309a2e0dd47608
kurt <kurtveil1@gmail.com>
2023-09-15 09:14:09 -0500

    [ADD]css and filter languages

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,30 +8,60 @@ import data from 'src/assets/data.json';
 export class HomeComponent {
   jobs: any[] = [];
   itemsFilter: any[] = [];
-  constructor(){
+  intersection: any;
+  languagesFiltered: any;
+  constructor() {
     this.jobs = data;
-    this.jobs.map(a=> {
-      if (a.logo){
+    this.jobs.map(a => {
+      if (a.logo) {
         const logo = a.logo;
         let newLogo = logo.slice(1);
         newLogo = `../../../assets${newLogo}`
         a.logo = newLogo;
       }
     });
-    
+    this.jobs.find(j => {
+      j.languages.push(j.role, j.level);
+      if (j.tools.length > 0) {
+        j.tools.map((a: any) => {
+          j.languages.push(a)
+        })
+      }
+
+    });
+
+
+    // console.warn(this.jobs); 
   }
 
-  clearFilter(){
+  clearFilter() {
     this.itemsFilter = [];
   }
 
-  removeItem(){
+  removeItem() {
     this.itemsFilter = [];
   }
 
-  filter(item: string){
+  filter(item: string) {
     this.itemsFilter.push(item);
-    console.warn(this.itemsFilter);
+    // console.warn(this.itemsFilter);
+  //  console.warn(this.filterForLanguage());
+   
+
+  }
+
+  filterForLanguage(){
+    if (this.itemsFilter.length > 0) {
+      this.jobs.find((a) => {
+         this.itemsFilter.filter((x: any) => {
+          if (a.languages.includes(x)){
+           
+                       
+          }
+        })
+      });
+    }
+    console.warn(this.languagesFiltered);
     
   }
 }
